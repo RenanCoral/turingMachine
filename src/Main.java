ced0275cded50800a52adc9025692529b5b2bf63
Wagner Ghedin <wagnerghedin10@gmail.com>
2020-07-01 22:06:04 -0300

    Criada tela principal

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class Main extends JFrame {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (UnsupportedLookAndFeelException e) {
+            // handle exception
+        }
+        catch (ClassNotFoundException e) {
+            // handle exception
+        }
+        catch (InstantiationException e) {
+            // handle exception
+        }
+        catch (IllegalAccessException e) {
+            // handle exception
+        }
+        new window().setVisible(true);
+    }
+}
